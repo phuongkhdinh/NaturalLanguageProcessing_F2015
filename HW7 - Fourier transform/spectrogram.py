6535835a9f4201a44b028086da6351a09e016a69
Phuong Dinh <dinhp@carleton.edu>
2015-11-01 11:06:53 -0600

    more graph correction

--- HW7 - Fourier transform/spectrogram.py
+++ HW7 - Fourier transform/spectrogram.py
@@ -14,17 +14,20 @@ def get_spectro(filename):
 	n = 0
 	while wav_file.tell() + 160 < wav_file.getnframes(): #spacing of window = 10ms = 160 frames
 		signal = np.fromstring(window, "int16")
-		T = 1.0 / 16000.0
 		fourier_transformed = np.fft.fft(signal)
+		# print(fourier_transformed)
+		# sys.exit()
 		spectroArray[n] = 10*np.log10(np.sqrt(np.square(fourier_transformed.real) + np.square(fourier_transformed.imag)))
+		#print(n,"--------------")
+		#print(spectroArray[n])
 		n += 1
 		wav_file.setpos(160 * n)		
 		window = wav_file.readframes(400)
-	print(spectroArray)
 
 	#Draw it
-
-	plt.imshow(spectroArray, cmap=cm.gist_gray) 
+	spectroArray = spectroArray.T
+	plt.imshow(spectroArray, origin = "lower", cmap = cm.Greys, extent=[0,n/100,0,16000], aspect='auto') 
+	# longitude_top_left,longitude_top_right,latitude_bottom_left,latitude_top_left
 	# erm, don't know which axis is which! timee need to be divided by 100
 	# don't know how to scale k back to frequency (by times 400)
 	# don't know how to make sure bigger is darker
@@ -40,7 +43,7 @@ def get_spectro(filename):
 
 def main():
 
-	wav_file = get_spectro("./timitwav/sx4.wav")
+	wav_file = get_spectro("./timitwav/sx14.wav")
 
 
 if __name__ == "__main__":
