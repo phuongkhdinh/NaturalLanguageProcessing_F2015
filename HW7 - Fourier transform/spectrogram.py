f7170b2bcf5a20ed865f156055bb20391f93d31a
Phuong Dinh <dinhp@carleton.edu>
2015-11-01 11:36:21 -0600

    input file changed format

--- HW7 - Fourier transform/spectrogram.py
+++ HW7 - Fourier transform/spectrogram.py
@@ -1,3 +1,9 @@
+'''
+	spectrogram.py
+	By Phuong Dinh and Julia Kroll
+	1 Nov 2015
+	Draw spectrogram of a wav file
+'''
 import matplotlib.pyplot as plt
 import numpy as np
 import wave
@@ -6,7 +12,12 @@ import matplotlib.cm as cm
 
 
 def get_spectro(filename):
-	wav_file = wave.open(filename, 'r')
+	try:
+		wav_file = wave.open(filename, 'r')
+	except:
+		print("ERROR: CANNOT OPEN INPUT FILE AS WAV FILE.")
+		sys.exit(1)
+		
 	timeStep = (wav_file.getnframes()-400) / 160
 	spectroArray = np.zeros(shape=(timeStep,400)) #spectroArray[t][k] = 10*log10(fourier_amplitude)
 
@@ -15,11 +26,8 @@ def get_spectro(filename):
 	while wav_file.tell() + 160 < wav_file.getnframes(): #spacing of window = 10ms = 160 frames
 		signal = np.fromstring(window, "int16")
 		fourier_transformed = np.fft.fft(signal)
-		# print(fourier_transformed)
-		# sys.exit()
 		spectroArray[n] = 10*np.log10(np.sqrt(np.square(fourier_transformed.real) + np.square(fourier_transformed.imag)))
-		#print(n,"--------------")
-		#print(spectroArray[n])
+
 		n += 1
 		wav_file.setpos(160 * n)		
 		window = wav_file.readframes(400)
@@ -27,28 +35,24 @@ def get_spectro(filename):
 	#Draw it
 	spectroArray = spectroArray.T
 	fig = plt.figure() 
-	plt.imshow(spectroArray, origin = "lower", cmap = cm.Greys, extent=[0,n/100,0,16000], aspect='auto') 
+
+	# We will only display half of the frequency, for the upper half is vertical mirror image of the lower one
+	plt.imshow(spectroArray[:len(spectroArray)/2], origin = "lower", cmap = cm.Greys, extent=[0,n/100,0,16000/2], aspect='auto') 
 	plt.xlabel("Time/s")
 	plt.ylabel("Frequency/Hz")
 	fig.suptitle('Spectrogram for ' + filename, fontsize=15)
-	# longitude_top_left,longitude_top_right,latitude_bottom_left,latitude_top_left
-	# erm, don't know which axis is which! timee need to be divided by 100
-	# don't know how to scale k back to frequency (by times 400)
-	# don't know how to make sure bigger is darker
 
 	plt.show()
 
-	# plt.figure(1)
-	# plt.title('Signal Wave...')
-	# plt.plot(signal)
-	#plt.show()
-	#userInput = input()
 
 
 def main():
 
-	wav_file = get_spectro("./timitwav/sx14.wav")
-
+	if len(sys.argv) == 2:
+		filename = sys.argv[1]
+		wav_file = get_spectro(filename)
+	else:
+		print("ERROR: INPUT WAV FILE IS MISSING.")
 
 if __name__ == "__main__":
 	main()
