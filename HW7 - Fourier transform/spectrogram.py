3081c429b6f37a40aa608ec8ccc9c32b5e717537
Julia Kroll <krollj@carleton.edu>
2015-11-01 13:30:07 -0600

    Tweaked plot's appearance and changed window background color to white

--- HW7 - Fourier transform/spectrogram.py
+++ HW7 - Fourier transform/spectrogram.py
@@ -18,28 +18,28 @@ def get_spectro(filename):
 		print("ERROR: CANNOT OPEN INPUT FILE AS WAV FILE.")
 		sys.exit(1)
 		
-	timeStep = (wav_file.getnframes()-400) / 160
-	spectroArray = np.zeros(shape=(timeStep,400)) #spectroArray[t][k] = 10*log10(fourier_amplitude)
+	time_step = (wav_file.getnframes()-400) / 160
+	spectro_array = np.zeros(shape=(time_step,400)) #spectro_array[t][k] = 10*log10(fourier_amplitude)
 
 	window = wav_file.readframes(400)
 	n = 0
 	while wav_file.tell() + 160 < wav_file.getnframes(): #spacing of window = 10ms = 160 frames
 		signal = np.fromstring(window, "int16")
 		fourier_transformed = np.fft.fft(signal)
-		spectroArray[n] = 10*np.log10(np.sqrt(np.square(fourier_transformed.real) + np.square(fourier_transformed.imag)))
+		spectro_array[n] = 10*np.log10(np.sqrt(np.square(fourier_transformed.real) + np.square(fourier_transformed.imag)))
 
 		n += 1
 		wav_file.setpos(160 * n)		
 		window = wav_file.readframes(400)
 
 	#Draw it
-	spectroArray = spectroArray.T
-	fig = plt.figure() 
+	spectro_array = spectro_array.T
+	fig = plt.figure(facecolor="white") 
 
 	# We will only display half of the frequency, for the upper half is vertical mirror image of the lower one
-	plt.imshow(spectroArray[:len(spectroArray)/2], origin = "lower", cmap = cm.Greys, extent=[0,n/100,0,16000/2], aspect='auto') 
-	plt.xlabel("Time/s")
-	plt.ylabel("Frequency/Hz")
+	plt.imshow(spectro_array[:len(spectro_array)/2], origin = "lower", cmap = cm.Greys, extent=[0,n/100,0,16000/2], aspect='auto') 
+	plt.xlabel("Time (seconds)")
+	plt.ylabel("Frequency (Hz)\n")
 	fig.suptitle('Spectrogram for ' + filename, fontsize=15)
 
 	plt.show()
