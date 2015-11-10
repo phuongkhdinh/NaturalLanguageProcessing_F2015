3b5edc9077d27ec783926c2a4eae2bf182a30acf
Julia Kroll <krollj@carleton.edu>
2015-11-10 10:47:12 -0600

    Convert unit productions into CNF

--- HW8 - Parser/parser.py
+++ HW8 - Parser/parser.py
@@ -14,30 +14,46 @@ def get_grammar(grammar_filename):
 		right_hand_side = divide_rule[1].strip().split(" ")
 
 		# Add rule to dictionary
-		if tuple(right_hand_side) in grammar_dict:
+		if tuple(right_hand_side) not in grammar_dict:
 			grammar_dict[tuple(right_hand_side)] = [left_hand_side]
 		else:
 			grammar_dict[tuple(right_hand_side)].append(left_hand_side)
-		if left_hand_side in lhs_rhs_dict:
+		if left_hand_side not in lhs_rhs_dict:
 			lhs_rhs_dict[left_hand_side] = [tuple(right_hand_side)]
 		else:
 			lhs_rhs_dict[left_hand_side].append(tuple(right_hand_side))
 
 	# If a rule is a unit production, duplicate rules to conform to CNF
+	lhs_deletion_list = []
+	grammar_deletion_list = []
 	for lhs in lhs_rhs_dict:
 		for rhs in lhs_rhs_dict[lhs]:
-			if len(rhs) == 1: # Unit production
+			if len(rhs) == 1 and rhs[0] != rhs[0].lower(): # Unit production
 				for possible_repeated_lhs in lhs_rhs_dict:
 					if possible_repeated_lhs == rhs:
-						lhs_rhs_dict[]
-			
+						for repeated_rule in lhs_rhs_dict[possible_repeated_lhs]:
+							lhs_rhs_dict[lhs].append(repeated_rule)
+							grammar_dict[repeated_rule].append(lhs)
+				grammar_dict[rhs].remove(lhs)
+				if grammar_dict[rhs] == []:
+					grammar_deletion_list.append(rhs)
+				lhs_rhs_dict[lhs].remove(rhs)
+				if lhs_rhs_dict[lhs] == []:
+					lhs_deletion_list.append(lhs)
+	for item in lhs_deletion_list:
+		lhs_rhs_dict.pop(item)
+	for item in grammar_deletion_list:
+		grammar_dict.pop(item)
 
+
+	print("LHS dict:\n", lhs_rhs_dict)
+	print("Grammar dict:\n", grammar_dict)
 	return grammar_dict
 
 def main():
 	grammar_filename = sys.argv[1]
 	grammar_dict = get_grammar(grammar_filename)
-	print(grammar_dict)
+	#print(grammar_dict)
 
 if __name__ == "__main__":
 	main()
