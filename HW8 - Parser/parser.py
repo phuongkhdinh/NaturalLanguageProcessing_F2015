34199a80fda261956ef2c59a364e32c72463809c
Julia Kroll <krollj@carleton.edu>
2015-11-10 11:09:48 -0600

    Full parser works on simple example

--- HW8 - Parser/parser.py
+++ HW8 - Parser/parser.py
@@ -1,29 +1,30 @@
 import sys
+
 class Node():
-	self.terminal = None
-	self.pointer = None
 	def __init__(self, terminal, tree):
 		self.terminal = terminal
 		self.pointer = tree
 
-
-
-def main(sentence, grammarRaw):
-	
-	grammar = getGrammar(grammarRaw)
+def main():
+	grammarRaw = sys.argv[1] # Filename
+	sentence = sys.argv[2]
+	grammar = get_grammar(grammarRaw)
 	tokens = sentence.split()
 	N = len(tokens) #N is number of words in sentence
 	# fill with 0
-	table = [[0 for i in range(N + 1)] for j in range(N+1)]
+	table = [[[] for i in range(N + 1)] for j in range(N+1)]
 	table = fillTable(table, tokens, grammar)
-	print(table[0,N])
+	for finalParse in table[0][N]:
+		if finalParse.terminal == "S":
 
 
+def printTree()
 def fillTable(table, tokens, grammar):
 	N = len(tokens)
+	print(grammar)
 	for i in range(N):
 		table[i][i+1] = []
-		productions = grammar[(tokens[i])]
+		productions = grammar[(tokens[i], )]
 		for production in productions:
 			table[i][i+1].append(Node(production, None)) #List of all possible terminals matching with token 
 	for i in range(1, N+1):
@@ -34,11 +35,12 @@ def fillTable(table, tokens, grammar):
 				for B in BList:
 					for C in CList: # Note: B and C are node object containing the terminal and its pointer
 						if (B.terminal, C.terminal) in grammar:
-							productions = grammar[(B, C)]
+							productions = grammar[(B.terminal, C.terminal)]
 							for production in productions:
 								table[j][i].append(Node(production, [B, C]))
 
 	return table 
+
 def get_grammar(grammar_filename):
 	"""Takes in a filename and returns a dictionary of rules in the format
 	   tuple of right hand side: string of left hand side"""
@@ -63,36 +65,45 @@ def get_grammar(grammar_filename):
 			lhs_rhs_dict[left_hand_side].append(tuple(right_hand_side))
 
 	# If a rule is a unit production, duplicate rules to conform to CNF
+	lhs_removal_list = []
 	lhs_deletion_list = []
+	grammar_removal_list = []
 	grammar_deletion_list = []
 	for lhs in lhs_rhs_dict:
 		for rhs in lhs_rhs_dict[lhs]:
 			if len(rhs) == 1 and rhs[0] != rhs[0].lower(): # Unit production
+				print("unit production rule:", lhs, "->", rhs)
 				for possible_repeated_lhs in lhs_rhs_dict:
 					if possible_repeated_lhs == rhs:
 						for repeated_rule in lhs_rhs_dict[possible_repeated_lhs]:
 							lhs_rhs_dict[lhs].append(repeated_rule)
 							grammar_dict[repeated_rule].append(lhs)
-				grammar_dict[rhs].remove(lhs)
-				if grammar_dict[rhs] == []:
+				grammar_removal_list.append((rhs, lhs))
+				#grammar_dict[rhs].remove(lhs)
+				if len(grammar_dict[rhs]) == 1: # Will be 0 after removal
 					grammar_deletion_list.append(rhs)
-				lhs_rhs_dict[lhs].remove(rhs)
-				if lhs_rhs_dict[lhs] == []:
+				lhs_removal_list.append((lhs, rhs))
+				#lhs_rhs_dict[lhs].remove(rhs)
+				if len(lhs_rhs_dict[lhs]) == 1: # Will be 0 after removal
 					lhs_deletion_list.append(lhs)
+	for item in lhs_removal_list:
+		lhs_rhs_dict[item[0]].remove(item[1])
 	for item in lhs_deletion_list:
 		lhs_rhs_dict.pop(item)
+	for item in grammar_removal_list:
+		grammar_dict[item[0]].remove(item[1])
 	for item in grammar_deletion_list:
 		grammar_dict.pop(item)
 
 
-	print("LHS dict:\n", lhs_rhs_dict)
-	print("Grammar dict:\n", grammar_dict)
+	#print("LHS dict:\n", lhs_rhs_dict)
+	#print("Grammar dict:\n", grammar_dict)
 	return grammar_dict
 
-def main():
-	grammar_filename = sys.argv[1]
-	grammar_dict = get_grammar(grammar_filename)
-	#print(grammar_dict)
+# def main():
+# 	grammar_filename = sys.argv[1]
+# 	grammar_dict = get_grammar(grammar_filename)
+# 	#print(grammar_dict)
 
 if __name__ == "__main__":
 	main()
