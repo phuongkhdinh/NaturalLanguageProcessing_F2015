08a47aa8aaac2ac1ed27aa44535f8ca3f6322c55
Julia Kroll <krollj@carleton.edu>
2015-11-22 22:51:10 -0600

    End sentence once a period is generated

--- Final project - Author emulator/predictor.py
+++ Final project - Author emulator/predictor.py
@@ -15,7 +15,7 @@ def generate_sentence(phrase):
 	top_selected_words, is_complete = Earley_Parser().parse(grammar["ROOT"], phrase, parser_probs, headword_bigram) # returns sorted dictionary of possible words and their probabilities
 	tokenized_phrase = word_tokenize(phrase)
 	#print(is_complete, len(tokenized_phrase), is_complete and len(tokenized_phrase) > min_length)
-	if (is_complete and len(tokenized_phrase) > min_length) or (len(top_selected_words) == 0) or (len(tokenized_phrase) > 12):
+	if (is_complete and len(tokenized_phrase) > min_length) or (len(top_selected_words) == 0) or (len(tokenized_phrase) > 12) or (tokenized_phrase[-1] == '.'):
 		#print(phrase, is_complete)
 		return phrase, is_complete
 	else:
@@ -41,7 +41,7 @@ def main():
 	while not is_complete and num_attempts < 6:
 		num_attempts += 1
 		phrase, is_complete = generate_sentence(starting_phrase)
-		sys.stdout.write("\nNot a legal sentence. Regenerate from beginning.")
+		sys.stdout.write("\nNot a legal sentence. Regenerate from beginning.\n")
 		sys.stdout.write("Sentence building: " + starting_phrase + " ")
 		sys.stdout.flush()
 	if phrase != "":
