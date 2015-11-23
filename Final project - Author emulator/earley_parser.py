30d3e060fef1a8679cf12d6b0e977e8377ac7198
Phuong Dinh <dinhp@carleton.edu>
2015-11-23 02:19:01 -0600

    increase variety

--- Final project - Author emulator/earley_parser.py
+++ Final project - Author emulator/earley_parser.py
@@ -176,7 +176,7 @@ class Earley_Parser():
             
         ### Rank word by probs
         predictWordProbMatrix = sorted(predictWordProbMatrix.items(), key=lambda x: x[1], reverse=True)
-        n = 4
+        n = 5
         topPredictedWord = predictWordProbMatrix[:min(n, len(predictWordProbMatrix))]
         #print(predictWordProbMatrix)
 
